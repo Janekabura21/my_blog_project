be142ed84fbd7fba142f648bb705e41d449e20e9
Janekabura21 <janenjoki2105@gmail.com>
2024-11-08 09:09:45 +0300

    Add home page view and template

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
+# Register the Post model with the admin panel
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at', 'updated_at')
     search_fields = ('title', 'content')
     list_filter = ('author', 'created_at')
     ordering = ('-created_at',)
-
 
 admin.site.register(Post, PostAdmin)
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def home(request):
+    # Query all posts, order by the latest created
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
